b5aa33a98d905b7af36227134d4890acc4deeaf0
Grégoire Sage <gregoire.sage@gmail.com>
2014-08-20 00:26:07 +0200

    manage subbitmap resizing

--- src/gbitmap_tools.c
+++ src/gbitmap_tools.c
@@ -4,31 +4,36 @@
 * Algorithm from http://www.compuphase.com/graphic/scale.htm and adapted for Pebble GBitmap
 */
 
-static void scaleRow(uint8_t *target, uint8_t *source, int srcWidth, int tgtWidth)
+static void scaleRow(uint8_t *target, uint8_t *source, int srcWidth, int tgtWidth, int srcOrigX)
 {
   int tgtPixels = 0;
   int intPart = srcWidth / tgtWidth;
   int fractPart = srcWidth % tgtWidth;
   int E = 0;
-  int srcIndex = 0;
+  int srcIndex = srcOrigX % 8;
+
+  source += srcOrigX / 8;
 
   while (tgtPixels < tgtWidth) {
     *target  |= ((*source >> srcIndex) & 1) << (tgtPixels % 8);
     srcIndex += intPart;
+    
     E += fractPart;
     if (E >= tgtWidth) {
       E -= tgtWidth;
       srcIndex++;
     } 
+
     if(srcIndex >= 8){
       source += srcIndex / 8;
       srcIndex = srcIndex % 8;
     }
+
     tgtPixels++;
     if(tgtPixels % 8 == 0){
      target++;
-   }
- } 
+    }
+  } 
 }
 
 GBitmap* scaleBitmap(GBitmap* src, uint8_t ratio_width_percent, uint8_t ratio_height_percent, uint8_t *resized_data){
@@ -37,10 +42,12 @@ GBitmap* scaleBitmap(GBitmap* src, uint8_t ratio_width_percent, uint8_t ratio_he
   if(ratio_width_percent <= 100 && ratio_height_percent <= 100){
     tgt = malloc(sizeof(GBitmap));
 
-    tgt->row_size_bytes = src->row_size_bytes;
     tgt->bounds = src->bounds;
-    tgt->bounds.size.h = tgt->bounds.size.h * ratio_height_percent / 100;
-    tgt->bounds.size.w = tgt->bounds.size.w * ratio_width_percent / 100;
+    tgt->bounds.origin.x = 0;
+    tgt->bounds.origin.y = 0;
+    tgt->bounds.size.h = src->bounds.size.h * ratio_height_percent / 100;
+    tgt->bounds.size.w = src->bounds.size.w * ratio_width_percent / 100;
+    tgt->row_size_bytes = tgt->bounds.size.w % 32 == 0 ? tgt->bounds.size.w / 4 : 4 * (tgt->bounds.size.w / 32) + 4;
     tgt->addr = resized_data;
     memset(tgt->addr, 0, (tgt->bounds.size.h * tgt->row_size_bytes) * sizeof(uint8_t));
 
@@ -56,11 +63,11 @@ GBitmap* scaleBitmap(GBitmap* src, uint8_t ratio_width_percent, uint8_t ratio_he
       int intPart = (srcHeight / tgtHeight) * src->row_size_bytes;
       int fractPart = srcHeight % tgtHeight;
       int E = 0;
-      uint8_t *source = src->addr;
+      uint8_t *source = src->addr + src->bounds.origin.y * src->row_size_bytes;
       uint8_t *target = tgt->addr;
 
       while (NumPixels-- > 0) {
-        scaleRow(target, source, srcWidth, tgtWidth);
+        scaleRow(target, source, srcWidth, tgtWidth, src->bounds.origin.x);
         target += tgt->row_size_bytes;
         source += intPart;
         E += fractPart;
