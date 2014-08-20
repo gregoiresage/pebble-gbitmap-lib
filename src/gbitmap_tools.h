33a36963c55f00a9dc9678f65fcec37488b00cb3
Grégoire Sage <gregoire.sage@gmail.com>
2014-08-21 00:27:29 +0200

    update documentation

--- src/gbitmap_tools.h
+++ src/gbitmap_tools.h
@@ -7,7 +7,6 @@
 //! @param src The source bitmap to be resized
 //! @param ratio_width_percent The width resize ratio (between 0 and 100)
 //! @param ratio_height_percent The height resize ratio (between 0 and 100)
-//! @param resized_data A pointer to the memory where the resized bitmap content is stored
 //! @return A pointer to the GBitmap. `NULL` if the GBitmap could not be created
 GBitmap* scaleBitmap(GBitmap* src, uint8_t ratio_width_percent, uint8_t ratio_height_percent);
 
